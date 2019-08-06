0426e9971fbeb360c446356ddf8430f90961614e
张雷 <lzhang@cheweilai.cn>
2019-08-06 17:14:25 +0800

    后台管理模板添加菜单栏的js

--- public/static/website-blue01/admin/scripts/menudropdown.js
+++ public/static/website-blue01/admin/scripts/menudropdown.js
@@ -2,18 +2,51 @@
 var menuDropDown=function () {
 
     var methods={
+        showErrInfo: function (val) {
+            var _errbox = $('<div></div>').addClass('errInfoBox');
+            _errbox.html(val).appendTo($('body'));
+            window.setTimeout(function () {
+                _errbox.remove();
+            }, 2000)
+        },
+        displayNavbar:function(querySelector){
+            console.log('displayNavbar',$(this));
+            console.log('displayNavbar',$(querySelector));
+            // $(".admin-aside").toggle();
+            if($(querySelector).hasClass('open')){
+                $(querySelector).removeClass('open');
+                $(".dislpayArrow").animate({ left:'14.215rem',opacity:1},10);
+                $(".wrapper").animate({ left:'14.215rem',opacity:1},100);
+                $('.admin-aside').show('slow');
+            }else{
+                $(querySelector).addClass('open');
+
+                $(".dislpayArrow").animate({ left:'0',opacity:1},10);
+                $(".wrapper").animate({ left:'0',opacity:1},100);
+                $('.admin-aside').hide('fast');
+            }
+
+        },
         init:function () {
-            // alert('init');
-            var vlist=document.querySelector(".menu_dropdown").querySelectorAll('dl');
+            // var vlist=document.querySelector(".menu_dropdown").querySelectorAll('dl');
+            var vlist=$(".menu_dropdown").find('dt');
             var array = [];
-            [].forEach.call(vlist, function(v) {
+            [].forEach.call(vlist, function(v) {//使用数据的foreach方法，将空数组替换成vlist；
                 array.push(v);
             });
             array.forEach(function(v,i){
                console.log('v',v);
                 v.onclick=function(e){
-                    // methods.currentMenu(this);
-                  /*  console.log($(this).hasClass('selected'));
+                   if( $(this).hasClass('selected')){
+                          $(this).removeClass('selected');
+                          $(this).siblings().css('display','none');
+                    }else{
+                        $(this).addClass('selected');
+                        $(this).siblings().css('display','block');
+                    }
+
+                 /*   // methods.currentMenu(this);
+                  /!*  console.log($(this).hasClass('selected'));
                     console.log($(this).children('dt'));
                     $(this).children('dt').onclick=function(){
                         if($(this).hasClass('selected')){
@@ -21,7 +54,7 @@ var menuDropDown=function () {
                             $(this).siblings().css('display','none');
 
                         }
-                    }*/
+                    }*!/
                     // if($(this).children('dt').hasClass('selected')){
                     //     // $(this).children('dt').removeClass('selected');
                     //     // $(this).children('dd').css('display','none');
@@ -31,7 +64,7 @@ var menuDropDown=function () {
                         $(this).siblings().children('dt').removeClass('selected');
                         $(this).siblings().children('dd').css('display','none');
 
-                    // }
+                    // }*/
                 };
             })
         },
@@ -46,7 +79,42 @@ var menuDropDown=function () {
                     alert($(this));
                 };
             })
-        }
+        },
+        getHomeData:function () {/*后台系统首页信息*/
+            $.ajax({
+                url: '/api/car/info',
+                type: 'POST',
+                data: {url:'/resource/cities/online'},
+                success:function (result) {
+
+                },
+                fail:function (err) {
+                    console.log(err);
+                    methods.showErrInfo(err.responseJSON.message);
+                }
+            });
+        },
+        getNews:function () {/*咨询管理*/
+
+        },
+        getProductsList:function () {/*产品管理*/
+
+        },
+        getBrandsList:function () {/*品牌管理*/
+
+        },
+        getCategoryList:function () {/*分类管理*/
+
+        },
+        getCommentsList:function () {/*评论管理*/
+
+        },
+        getMemberList:function () {/*会员管理-会员列表*/
+
+        },
+        getAdministrator:function () {/*管理员管理*/
+
+        },
     };
     return methods;
 };
