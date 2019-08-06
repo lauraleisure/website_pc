d3598a4c1cd98101c1e3cc1622bbb78eb40f8258
张雷 <lzhang@cheweilai.cn>
2019-08-06 09:01:43 +0800

    后台管理模板

--- public/static/website-blue01/admin/scripts/menudropdown.js
+++ public/static/website-blue01/admin/scripts/menudropdown.js
@@ -3,7 +3,49 @@ var menuDropDown=function () {
 
     var methods={
         init:function () {
-            
+            // alert('init');
+            var vlist=document.querySelector(".menu_dropdown").querySelectorAll('dl');
+            var array = [];
+            [].forEach.call(vlist, function(v) {
+                array.push(v);
+            });
+            array.forEach(function(v,i){
+               console.log('v',v);
+                v.onclick=function(e){
+                    // methods.currentMenu(this);
+                  /*  console.log($(this).hasClass('selected'));
+                    console.log($(this).children('dt'));
+                    $(this).children('dt').onclick=function(){
+                        if($(this).hasClass('selected')){
+                            $(this).removeClass('selected');
+                            $(this).siblings().css('display','none');
+
+                        }
+                    }*/
+                    // if($(this).children('dt').hasClass('selected')){
+                    //     // $(this).children('dt').removeClass('selected');
+                    //     // $(this).children('dd').css('display','none');
+                    // }else{
+                        $(this).children('dt').addClass('selected').siblings().removeClass('selected');
+                        $(this).children('dd').css('display','block');
+                        $(this).siblings().children('dt').removeClass('selected');
+                        $(this).siblings().children('dd').css('display','none');
+
+                    // }
+                };
+            })
+        },
+        currentMenu:function (querySelector) {
+            var vlist=document.querySelector(querySelector).querySelectorAll('li');
+            var array = [];
+            [].forEach.call(vlist, function(v) {
+                array.push(v);
+            });
+            array.forEach(function(v,i){
+                v.onclick=function(e){
+                    alert($(this));
+                };
+            })
         }
     };
     return methods;
